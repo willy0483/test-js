384b5e5491314512e21580378a57ca8685af6713
willy0483 <145011279+willy0483@users.noreply.github.com>
2024-04-15 12:10:51 +0200

    Initial commit

---
+++ assets/js/HEADlink.js
@@ -0,0 +1,15 @@
+// Link me in the header
+
+console.group('HEADlink.js')
+
+
+console.info('Jeg er HEADlink.js');
+
+// find dom element med id
+const myHiddenElement = document.getElementById('hidden');
+
+console.warn(myHiddenElement.innerText);
+
+
+
+console.groupEnd()
---
+++ assets/js/HTMLlink.js
@@ -0,0 +1,16 @@
+// Link me in HTML
+
+
+console.group('HTMLlink.js')
+
+
+console.info('Jeg er HTMLlink.js');
+
+// find dom element med id
+const HiddenElement = document.getElementById('hidden');
+
+console.warn(HiddenElement.innerText);
+
+
+
+console.groupEnd()
